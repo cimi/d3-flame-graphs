3dd747967f8c78164df3b9ee969f51b44626378c
CircleCI <circleci@circleci>
2015-10-11 11:19:53 +0000

    Update (2015-10-11 11:19:53 UTC) [ci skip]

--- d3-flame-graph.js
+++ d3-flame-graph.js
@@ -8,12 +8,30 @@
   }
 
   d3.flameGraph = function() {
-    var FlameGraph;
+    var FlameGraph, getClassAndMethodName, hash;
+    getClassAndMethodName = function(fqdn) {
+      var tokens;
+      tokens = fqdn.split(".");
+      return tokens.slice(tokens.length - 2).join(".");
+    };
+    hash = function(name) {
+      var i, j, maxHash, mod, ref, ref1, result, weight;
+      ref = [0, 0, 1, 10], result = ref[0], maxHash = ref[1], weight = ref[2], mod = ref[3];
+      name = getClassAndMethodName(name).slice(0, 6);
+      for (i = j = 0, ref1 = name.length - 1; 0 <= ref1 ? j <= ref1 : j >= ref1; i = 0 <= ref1 ? ++j : --j) {
+        result += weight * (name.charCodeAt(i) % mod);
+        maxHash += weight * (mod - 1);
+        weight *= 0.7;
+      }
+      if (maxHash > 0) {
+        return result / maxHash;
+      } else {
+        return result;
+      }
+    };
     FlameGraph = (function() {
-      var getClassAndMethodName;
-
       function FlameGraph() {
-        this._generateAccessors(['size', 'margin', 'cellHeight', 'breadcrumbs', 'tooltip', 'colors']);
+        this._generateAccessors(['size', 'margin', 'cellHeight', 'breadcrumbs', 'tooltip', 'color']);
         this._allData = [];
         this._size = [1200, 800];
         this._cellHeight = 10;
@@ -23,7 +41,14 @@
           bottom: 0,
           left: 0
         };
-        this._colors = ['rgb(255,255,178)', 'rgb(254,204,92)', 'rgb(253,141,60)', 'rgb(240,59,32)', 'rgb(189,0,38)'];
+        this._color = function(d) {
+          var b, g, r, val;
+          val = hash(d.name);
+          r = 200 + Math.round(55 * val);
+          g = 0 + Math.round(230 * (1 - val));
+          b = 0 + Math.round(55 * (1 - val));
+          return "rgb(" + r + ", " + g + ", " + b + ")";
+        };
       }
 
       FlameGraph.prototype.data = function(data) {
@@ -33,6 +58,12 @@
         this._allData.push(data);
         this.totalSamples = data.samples;
         this._data = d3.layout.partition().sort(function(a, b) {
+          if (!a.name) {
+            return 1;
+          }
+          if (!b.name) {
+            return -1;
+          }
           return a.name.localeCompare(b.name);
         }).nodes(data);
         return this;
@@ -55,21 +86,12 @@
         return label.substr(0, Math.round(this.x(d.dx) / 4));
       };
 
-      FlameGraph.prototype.color = function(d) {
-        return this.colors()[Math.floor(Math.random() * this.colors().length)];
-      };
-
-      getClassAndMethodName = function(fqdn) {
-        var tokens;
-        tokens = fqdn.split(".");
-        return tokens.slice(tokens.length - 2).join(".");
-      };
-
       FlameGraph.prototype.render = function(selector) {
+        var nodes;
         console.time('render');
         this._selector = selector;
         d3.select(selector).select('svg').remove();
-        this.container = d3.select(selector).append('svg').attr('width', this.size()[0]).attr('height', this.size()[1]).style('border', '1px solid #0e0e0e').append('g').attr('transform', "translate(" + (this.margin().left) + ", " + (this.margin().top) + ")");
+        this.container = d3.select(selector).append('svg').attr('class', 'flame-graph').attr('width', this.size()[0]).attr('height', this.size()[1]).append('g').attr('transform', "translate(" + (this.margin().left) + ", " + (this.margin().top) + ")");
         this.maxCells = Math.floor(this.height() / this.cellHeight());
         this.maxDepth = this.data()[0].maxDepth;
         this.x = d3.scale.linear().domain([
@@ -86,17 +108,18 @@
             return (cell - _this.maxDepth + _this.maxCells) * _this.cellHeight();
           };
         })(this)));
-        this.container.selectAll('.node').data(this.data().filter((function(_this) {
+        nodes = this.container.selectAll('.node').data(this.data().filter((function(_this) {
           return function(d) {
-            return _this.x(d.dx) > 0.1 && _this.y(d.y) >= 0 && d.type !== 'filler';
+            return _this.x(d.dx) > 0.1 && _this.y(d.y) >= 0 && !d.filler;
           };
-        })(this))).enter().append('rect').attr('class', 'node').attr('width', (function(_this) {
+        })(this))).enter().append('g').attr('class', 'node');
+        nodes.append('rect').attr('width', (function(_this) {
           return function(d) {
             return _this.x(d.dx);
           };
         })(this)).attr('height', (function(_this) {
           return function(d) {
-            return _this.cellHeight() - 2;
+            return _this.cellHeight();
           };
         })(this)).attr('x', (function(_this) {
           return function(d) {
@@ -106,18 +129,18 @@
           return function(d) {
             return _this.y(d.y);
           };
-        })(this)).attr('stroke', this.colors()[this.colors().length - 1]).attr('fill', (function(_this) {
+        })(this)).attr('fill', (function(_this) {
           return function(d) {
-            return _this.color(d);
+            return _this.color()(d);
           };
-        })(this)).attr('fill-opacity', '0.8');
-        this.container.selectAll('.label').data(this.data().filter((function(_this) {
+        })(this));
+        nodes.append('text').attr('class', 'label').attr('width', (function(_this) {
           return function(d) {
-            return d.name && _this.x(d.dx) > 40;
+            return _this.x(d.dx);
           };
-        })(this))).enter().append('text').attr('class', 'label').attr('text-anchor', 'middle').attr('dy', '.25em').attr('x', (function(_this) {
+        })(this)).attr('dy', '.25em').attr('x', (function(_this) {
           return function(d) {
-            return _this.x(d.x) + _this.x(d.dx) / 2;
+            return _this.x(d.x) + 2;
           };
         })(this)).attr('y', (function(_this) {
           return function(d) {
@@ -125,9 +148,28 @@
           };
         })(this)).text((function(_this) {
           return function(d) {
-            return _this.label(d);
+            if (d.name && _this.x(d.dx) > 40) {
+              return _this.label(d);
+            }
           };
         })(this));
+        nodes.append('rect').attr('width', (function(_this) {
+          return function(d) {
+            return _this.x(d.dx);
+          };
+        })(this)).attr('height', (function(_this) {
+          return function(d) {
+            return _this.cellHeight();
+          };
+        })(this)).attr('x', (function(_this) {
+          return function(d) {
+            return _this.x(d.x);
+          };
+        })(this)).attr('y', (function(_this) {
+          return function(d) {
+            return _this.y(d.y);
+          };
+        })(this)).attr('opacity', 0);
         console.timeEnd('render');
         console.log("Rendered " + (this.container.selectAll('.node')[0].length) + " elements");
         if (this.breadcrumbs()) {
@@ -157,10 +199,32 @@
             }
             return 'n';
           };
-        })(this)).offset([-this.cellHeight() / 2, 0]);
+        })(this)).offset((function(_this) {
+          return function(d) {
+            var x, xOffset, yOffset;
+            x = _this.x(d.x) + _this.x(d.dx) / 2;
+            xOffset = _this.x(d.dx) / 2;
+            yOffset = _this.cellHeight() / 2;
+            if (_this.width() - 100 < x) {
+              return [0, -xOffset];
+            }
+            if (x < 100) {
+              return [0, xOffset];
+            }
+            if (_this.y(d.y) < 100) {
+              return [yOffset, 0];
+            }
+            return [-yOffset, 0];
+          };
+        })(this));
         this.container.call(this.tip);
-        this.container.selectAll('.node').on('mouseover', this.tip.show).on('mouseout', this.tip.hide);
-        this.container.selectAll('.label').on('mouseover', this.tip.show).on('mouseout', this.tip.hide);
+        this.container.selectAll('.node').on('mouseover', (function(_this) {
+          return function(d) {
+            console.log(d, d3.event);
+            d3.event.stopPropagation();
+            return _this.tip.show(d);
+          };
+        })(this)).on('mouseout', this.tip.hide);
         return this;
       };
 
@@ -193,11 +257,7 @@
       FlameGraph.prototype._enableNavigation = function() {
         this.container.selectAll('.node').on('click', (function(_this) {
           return function(d) {
-            return _this.data(d).render(_this._selector);
-          };
-        })(this));
-        this.container.selectAll('.label').on('click', (function(_this) {
-          return function(d) {
+            d3.event.stopPropagation();
             return _this.data(d).render(_this._selector);
           };
         })(this));
@@ -205,10 +265,10 @@
       };
 
       FlameGraph.prototype._generateAccessors = function(accessors) {
-        var accessor, i, len, results;
+        var accessor, j, len, results;
         results = [];
-        for (i = 0, len = accessors.length; i < len; i++) {
-          accessor = accessors[i];
+        for (j = 0, len = accessors.length; j < len; j++) {
+          accessor = accessors[j];
           results.push(this[accessor] = (function(accessor) {
             return function(newValue) {
               if (!arguments.length) {

39164c95f5d6f6f5fdcdf63c0c3ccd56f9c78891
Alex Ciminian <alex.ciminian@gmail.com>
2017-05-16 23:17:38 +0100

    Fix test runner and add dummy test using ES6

---
+++ test/sample.js
@@ -0,0 +1,8 @@
+import chai from 'chai';
+
+describe('when writing ES6 tests', () => {
+  it('will execute source and test code correctly', () => {
+    const expect = chai.expect;
+    expect(true).to.be.true;
+  });
+})
